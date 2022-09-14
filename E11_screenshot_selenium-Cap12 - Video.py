12a500d54a6c34703d45a12347ddc7c11a23c97b
GerardoUroz <elchaca2007@gmail.com>
2022-09-14 17:48:24 -0300

    usando unitest - Usando Toggle - HTML

--- E11_screenshot_selenium-Cap12 - Video.py
+++ E11_screenshot_selenium-Cap12 - Video.py
@@ -14,34 +14,34 @@ class usando_unittest(unittest.TestCase):
         self.driver = webdriver.Chrome(executable_path=r"C:\DriverChrome\chromedriver.exe")
 
 
-def test_opencv(self):
-    driver = self.driver
-    driver.get("http://www.google.com")
-    driver.save_screenshot('img2.png') #guardar la imagen de comparacion en png
-    time.sleep(3)
+    def test_opencv(self):
+        driver = self.driver
+        driver.get("http://www.google.com")
+        driver.save_screenshot('img2.png') #guardar la imagen de comparacion en png
+        time.sleep(3)
 
-def test_comparandpo_imagenes(self):
-    img1 = cv2.imgread('img1.png')
-    img2 = cv2.imgread('img2.png')
+    def test_comparandpo_imagenes(self):
+        img1 = cv2.imread('img1.png')
+        img2 = cv2.imread('img2.png')
 
-    diferencia = cv2.absdiff(img1,img2)
-    imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
-    contours,_ = cv2.findContours(imagen_gris, cv2.RETR_EXTERNAL, cv2.CHAIN_APROX_SIMPLE)
+        diferencia = cv2.absdiff(img1,img2)
+        imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
+        contours,_ = cv2.findContours(imagen_gris, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for c in contours:
-        if cv2.contourArea(c) >= 20:
-             posicion_x, posicion_y, ancho, alto = cv2.boundingRect(c)
-             cv2.rectangle(img1,(posicion_x, posicion_y), (posicion_x+ancho, posicion_y+alto), (0,0,255),2 )
+        for c in contours:
+            if cv2.contourArea(c) >= 20:
+                posicion_x, posicion_y, ancho, alto = cv2.boundingRect(c)
+                cv2.rectangle(img1,(posicion_x, posicion_y), (posicion_x+ancho, posicion_y+alto), (0,0,255),2 )
 
-    while (1):
-        cv2.imshow('Imagen1', img1)
-        cv2.imshow('Imagen2',img2)
-        cv2.imshow('Diferencias detectadas', diferencia)
-        teclado = cv2.waitKey(5) & 0xFF
-        if teclado == 27:
-            break
+        while (1):
+            cv2.imshow('Imagen1', img1)
+            cv2.imshow('Imagen2', img2)
+            cv2.imshow('Diferencias detectadas', diferencia)
+            teclado = cv2.waitKey(5) & 0xFF
+            if teclado == 27:
+                break
     
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
         
 if __name__ == '__main__':
     unittest.main()
